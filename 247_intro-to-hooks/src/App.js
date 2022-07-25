e8060cf06a871d8657222a68bade1755696a2a14
HeatherH <hherries@yahoo.com>
2022-07-25 14:29:21 -0600

    Built my first hook

--- 247_intro-to-hooks/src/App.js
+++ 247_intro-to-hooks/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import CounterClass from "./CounterClass";
+import CounterHooks from "./CounterHooks";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CounterClass />
+      <CounterHooks />
+    </div>
+  );
 }
 
 export default App;
