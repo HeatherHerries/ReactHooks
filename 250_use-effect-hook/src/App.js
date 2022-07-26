6bf3cd65e35e601dbcd9e86b41883be1b0cb6068
HeatherH <hherries@yahoo.com>
2022-07-26 16:43:22 -0600

    Created my first useEffect Hook

--- 250_use-effect-hook/src/App.js
+++ 250_use-effect-hook/src/App.js
@@ -1,7 +1,12 @@
+import Clicker from "./Clicker";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Clicker />
+    </div>
+  );
 }
 
 export default App;
